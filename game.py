3f453c64c5a07f7f9f35ce28ecc9028b47cb445d
Dilraaj-Gill <91950496+Dilraaj-Gill@users.noreply.github.com>
2023-05-08 10:59:15 +0100

    Vector python file added for operations

--- game.py
+++ game.py
@@ -5,3 +5,11 @@ except ImportError:
 
 import random
 import time
+
+class Spritesheet:
+    def __init__(self, sprite_url, dest_centre, velocity):
+        self.sprite_url = sprite_url
+        self.dest_centre = dest_centre
+        self.velocity = velocity
+    
+    
